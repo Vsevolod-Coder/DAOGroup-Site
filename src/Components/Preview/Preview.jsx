4ff95cc3d43bd05559e7d81ed4439dca3b86941e
vsevolod l <vsevolod@daolab.pro>
2025-01-16 17:44:51 +0600

    preview done

--- src/Components/Preview/Preview.jsx
+++ src/Components/Preview/Preview.jsx
@@ -6,9 +6,9 @@ const Preview = () => {
     <section className='Preview'>
       <div className='Preview_wrapper'>
         <div className='Preview-content'>
-            <h1>Сервис - ориентированный
+            <h1>Сервис - ориентированный <br />
             системный интегратор</h1>
-            <h2 className='Preview-h2'>Мы создаем мир безграничных возможностей для развития бизнеса наших заказчиков,
+            <h2 className='Preview-h2'>Мы создаем мир безграничных возможностей для развития бизнеса наших заказчиков, <br />
             используя самые передовые технологии и новаторские идеи</h2>
             <button className='Preview-button'>Получить консультацию</button>
         </div>
